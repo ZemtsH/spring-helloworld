7adfdf590f3651df0758a6f0a1eb4c4867e835d7
Helya <z>
2021-11-17 15:47:32 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+    }
+}
